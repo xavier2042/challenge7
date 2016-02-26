73b89bf952925a177faed86ca1f035332238df7f
xavier2042 <xavier2042@gmail.com>
2016-02-26 09:21:51 +0100

    integration de slick

--- js/script.js
+++ js/script.js
@@ -3,12 +3,22 @@ $(document).ready(function() {
   $("#owl-demo").owlCarousel({
 
       autoPlay: 1000, //Set AutoPlay to 3 seconds
-
       items : 3,
       itemsDesktop : [1199,3],
       itemsDesktopSmall : [979,3]
 
   });
-  
+
+
+  $(window).fadeThis({
+    distance: 200,
+  });
+  $(".fleche").hover(function() {
+    $(this).addClass('animated bounce infinite');
+  },
+    function() {
+    $(this).removeClass('animated bounce infinite');
+    });
+
 
 });
